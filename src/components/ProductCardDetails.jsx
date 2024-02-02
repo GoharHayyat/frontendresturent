cc69368e9a66277406fdeddf092bda87e5b28cd6
GoharHayyat <goharchisthi@gmail.com>
2024-02-02 07:59:07 +0500

    checking

--- src/components/ProductCardDetails.jsx
+++ src/components/ProductCardDetails.jsx
@@ -17,6 +17,7 @@ function ProductCardDetails({product}) {
        {product.describtion}
        <br/>
        {product.carbohydrates}
+       {product.protein}
       </div>
     );
   
--- src/pages/ProductsListPage.jsx
+++ src/pages/ProductsListPage.jsx
@@ -6,6 +6,8 @@ import LoadingScreen from "./LoadingScreen";
 
 import Checkbox from "@mui/joy/Checkbox";
 
+//Main Page for sending product data
+
 function ProductsListPage() {
   const { category, searchQuery, brand } = useParams();
   const [products, setProducts] = useState([]);
